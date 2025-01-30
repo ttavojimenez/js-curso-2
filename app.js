b0213ff4a3f99f0886776fa36256a0ce9f322b65
ttavojimenez <ttavojimenez26@gmail.com>
2025-01-29 23:54:10 -0500

    Resultado de practica
    se realizan todos los ejercicios dejados para practicar

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+let titulo = document.querySelector ('h1');
+titulo.innerHTML = "Hora del Desafio";
 
+function doyClick() {
+    console.log ("El Boton fue clicado");
+}
+
+function preguntarCiudad() {
+    const ciudad = prompt("Por favor ingresa una ciudad de brasil : ")
+
+    if (ciudad) {
+        alert(`Estuve en ${ciudad} y me acordé de ti`);
+    }else{
+        alert("No ingresaste una ciudad valida");
+    }
+}
+
+function yoAmo(){
+    alert("Yo amo a Js");
+}
+
+function sumandonos(){
+    const numero1 = parseFloat(prompt("Digita el primer numero"));
+    const numero2 = parseFloat(prompt("Digita el segundo numero:"));
+
+    const resultado = numero1 + numero2;
+
+    alert(`La suma de los numeros es : ${resultado}`);
+}
